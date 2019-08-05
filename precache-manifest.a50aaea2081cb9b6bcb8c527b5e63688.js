5c2412a00dc36b3ff4bb8ad47a28877761b0819a
Your Name <griseldavcardozo@gmail.com>
2019-08-05 16:21:23 -0300

    Updates

---
+++ precache-manifest.a50aaea2081cb9b6bcb8c527b5e63688.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bfe1055d9fdd009178fe88c35607916",
+    "url": "/tareas-react-app/index.html"
+  },
+  {
+    "revision": "0f0cf58249165a9bb30b",
+    "url": "/tareas-react-app/static/css/main.7c5ecd73.chunk.css"
+  },
+  {
+    "revision": "ae05e698d873c9f739e5",
+    "url": "/tareas-react-app/static/js/2.853cd350.chunk.js"
+  },
+  {
+    "revision": "0f0cf58249165a9bb30b",
+    "url": "/tareas-react-app/static/js/main.f4960848.chunk.js"
+  },
+  {
+    "revision": "5b5e8fa31a796ca88e4a",
+    "url": "/tareas-react-app/static/js/runtime~main.1d208bf8.js"
+  }
+]);
